6a6078b679dae3e9caa3f429883f9287b2f48133
Nurlan <iskakov9821@outlook.com>
2025-02-22 01:40:38 +0700

    updates

--- cinema-app/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import style from "./Footer.module.css";
-
-export const Footer = () => {
-  return (
-    <div className={style.footer_container}>
-      <p>FOOTER</p>
-    </div>
-  );
-};
--- cinema-app/src/components/Main.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router-dom";
-import style from "./Main.module.css";
-export const Main = () => {
-  return (
-    <div className={style.main}>
-      <Outlet />
-    </div>
-  );
-};
