65be8ed07705fd987ac0d5d559e706979c383520
Nurlan <iskakov9821@outlook.com>
2025-03-10 00:41:32 +0700

    update footer

--- Cinemate/src/components/Footer.tsx
+++ Cinemate/src/components/Footer.tsx
@@ -1,9 +1,24 @@
 import style from "./Footer.module.css";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
     <div className={style.footer_container}>
-      <p>FOOTER</p>
+      <div>
+        <p>O Нас:</p>
+        <p>
+          Связь с тех поддержкой:{" "}
+          <strong>cinemate.tekhsupport@gmail.com</strong>
+        </p>
+        <p>
+          Вакансии: <strong>cinemate.job@gmail.com</strong>
+        </p>
+      </div>
+      <div>
+        <p>Cервис:</p>
+        <p>Регистрация {<Link to="">КЛИК</Link>}</p>
+        <p>Оформление подписки {<Link to="">КЛИК</Link>}</p>
+      </div>
     </div>
   );
 };
